5be7941c0a2183c03f0dd03ab0b667f2c61a6c7b
jordancuadrado <jac59155@gmail.com>
2024-09-09 21:36:07 -0400

    updated

--- index.js
+++ index.js
@@ -18,7 +18,10 @@ const checkIfFileExists = () => {
 const add = (description) => {
     let obj = { table : [] };
 
-    obj = checkIfFileExists();
+    if(fs.existsSync('data.json')) {
+        const data = fs.readFileSync("data.json");
+        obj = JSON.parse(data);
+    } 
 
     obj.table.push({
         id: obj.table.length + 1,
@@ -60,9 +63,15 @@ const update = (id, description) => {
     let findIdIndex = obj.table.findIndex((item) => item.id === id);
     if(findIdIndex === -1) {
         console.log('Task not found');
-    } else {        
+    } else {     
+        
         obj.table[findIdIndex].description = description;
         obj.table[findIdIndex].updatedAt = new Date;
+
+
+        let json = JSON.stringify(obj, null, 2);
+        fs.writeFileSync('data.json', json);
+        
         console.log(`Task ${id} updated!`);
     }
 }
@@ -75,11 +84,14 @@ const list = () => {
 
 switch (args[0]) {
     case "add":
-        add();
+        const newDescription = args[1];
+        add(newDescription);
         break;
     case "update":
         const idToUpdate = parseInt(args[1], 10);
         const description = args[2];
+
+        console.log(description);
         update(idToUpdate, description);
         break;
     case "delete":
